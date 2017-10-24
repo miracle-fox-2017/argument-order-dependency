6ca6354fc9dd97db8e99595986eaff884fa24d1f
Nandira Paturohman <nandirasp@gmail.com>
2017-10-24 18:36:41 +0700

    argument order dependency Done

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(houseData) {
+    this.address = houseData.address
+    this.square_feet = houseData.square_feet
+    this.num_bedrooms = houseData.num_bedrooms || 3
+    this.num_baths = houseData.num_baths || 2
+    this.cost = houseData.cost || 320000
+    this.down_payment = houseData.down_payment || 0.20
+    this.sold = houseData.sold || false
+    this.short_sale = houseData.short_sale
+    this.has_tenants = houseData.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g,'****')
   }
 
   buy(money, good_credit) {
@@ -33,6 +33,19 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let houseData = {
+  address : 'address',
+  square_feet : 100,
+  num_bedrooms : 2,
+  num_baths : 2,
+  cost : 12345,
+  down_payment: 12345,
+  sold : true,
+  short_sale : true
+}
 
+// const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const cool = new House(houseData)
+
+cool.buy
 console.log(cool.to_s())
