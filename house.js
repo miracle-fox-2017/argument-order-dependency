f6751fb512a6983a35e514ae5f0baf5c98d50a20
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-24 15:13:45 +0700

    Rilis 0

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(args) {
+    this.address = args.address
+    this.square_feet = args.square_feet
+    this.num_bedrooms = args.num_bedrooms || 3
+    this.num_baths = args.num_baths || 2
+    this.cost = args.cost || 320000
+    this.down_payment = args.down_payment || 0.20
+    this.sold = args.sold || false
+    this.short_sale = args.short_sale
+    this.has_tenants = args.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -33,6 +33,17 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const args = {
+    address : 'Jl. Bank no. 5',
+    square_feet : '100',
+    num_bedrooms : 2,
+    num_baths : 2,
+    cost : 320000,
+    down_payment : 0.20,
+    sold : false,
+    short_sale : true,
+    has_tenants : true
+}
+const cool = new House(args)
 
 console.log(cool.to_s())
