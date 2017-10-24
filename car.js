857d53d2bbd70451735e139dfa52268d0ce4c032
Tomy Djaya Budiman <tommybudi1998@gmail.com>
2017-10-24 17:20:56 +0700

    Argument Order Dependency

--- car.js
+++ car.js
@@ -1,32 +1,33 @@
 'use strict'
 
-class Car {
-
-  start_car(options) {
-    if (options && options['headlights']) {
-      this.headlights = options['headlights']
-    } else {
-      this.headlights = true
+class Car{
+    start_car(options){
+        if(options && options.hasOwnProperty("headlights")){ // Jika options
+            this.headlights = options['headlights'];
+        }else{
+            this.headlights = true
+        }
+        //
+        // if(options && options['gear']){
+        //     this.gear = options['gear']
+        // }else{
+        //     this.gear = 1
+        // }
     }
-
-    if (options && options['gear']) {
-      this.gear = options['gear']
-    } else {
-      this.gear = 1
-    }
-  }
 }
 
 const herbie = new Car()
 
 // When we start the car, the headlights should be on by default
-herbie.start_car()
+herbie.start_car();
 
 console.log(herbie.headlights) // true
 
 // But what happens when we want to start without headlights?
 herbie.start_car({
-  headlights: false
-})
+    headlights: false
+});
 
 console.log(herbie.headlights) // true ...KOK BISA?!
+
+// Penjelasan ==> Karena di dalam if clause terdapat statement jika options tidak kosong dan properties dari options sama dengan true maka properties headlights akan berubah sesuai input parameter, sedangkan ketika kita memanggil method start_car kita memasukan value false pada key headlights.
--- house.js
+++ house.js
@@ -1,38 +1,49 @@
 'use strict'
 
 class House {
-
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
-  }
-
-  obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
-  }
-
-  buy(money, good_credit) {
-    if (money >= down_payment && good_credit) {
-      this.sold = true
+    constructor(house){
+        this.address=house.address
+        this.square_feet=house.squareFeet
+        this.num_bedrooms=house.numBedrooms || 3
+        this.num_baths=house.numBaths || 2
+        this.cost=house.cost || 320000
+        this.down_payment=house.downPayment || 0.20
+        this.sold=house.sold || false
+        this.short_sale=house.shortSale
+        this.has_tenants=house.hasTenants || false
     }
-  }
 
-  down_payment() {
-    return cost * this.down_payment
-  }
+    obscure_address(){
+        return this.address.replace(/.{10}$/g,"****");
+    }
 
-  to_s() {
-    return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
-  }
+    buy(money,good_credit){
+        if(money >= down_payment && good_credit){
+            this.sold = true
+        }
+    }
+
+    down_payment(){
+        return cost * this.down_payment
+    }
+
+    to_s(){
+        return `${this.obscure_address()} : ${this.square_feet} sq. ft., ${this.num_bedrooms} bed, ${this.num_baths} bath. ${this.cost}`
+    }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+const param={
+    address:"onyetenyevwe",
+    squareFeet:150,
+    numBedrooms:3,
+    numBaths:2,
+    cost:1300000000,
+    downPayment:300000000,
+    sold:1250000000,
+    shortSale:true,
+    hasTenants:true
 
-console.log(cool.to_s())
+}
+const cool = new House(param);
+
+console.log(cool.to_s());
