a8f440e7a9c1218571ab7a5ebc35a24a9c68f8ef
fabiokounang <fabio_kosukeueki@yahoo.com>
2017-10-24 20:06:33 +0700

    Tugas DEPENDENCY

--- house.js
+++ house.js
@@ -2,20 +2,20 @@
 
 class House {
 
-  constructor(address, square_feet, num_bedrooms, num_baths, cost, down_payment, sold, short_sale, has_tenants) {
-    this.address = address
-    this.square_feet = square_feet
-    this.num_bedrooms = num_bedrooms || 3
-    this.num_baths = num_baths || 2
-    this.cost = cost || 320000
-    this.down_payment = down_payment || 0.20
-    this.sold = sold || false
-    this.short_sale = short_sale
-    this.has_tenants = has_tenants || false
+  constructor(objek) {
+    this.address = objek.address
+    this.square_feet = objek.square_feet
+    this.num_bedrooms = objek.num_bedrooms || 3
+    this.num_baths = objek.num_baths || 2
+    this.cost = objek.cost || 320000
+    this.down_payment = objek.down_payment || 0.20
+    this.sold = objek.sold || false
+    this.short_sale = objek.short_sale
+    this.has_tenants = objek.has_tenants || false
   }
 
   obscure_address() {
-    this.address.replace(/.{10}$/g, '****')
+    return this.address.replace(/.{10}$/g, '****')
   }
 
   buy(money, good_credit) {
@@ -33,6 +33,18 @@ class House {
   }
 }
 
-const cool = new House('address', 100, 2, 2, 12345, 12345, true, true)
+let objek = {
+  address      : 'address',
+  square_feet  : 100,
+  num_bedrooms : 2,
+  num_baths    : 2,
+  cost         : 12345,
+  down_payment : 12345,
+  sold         : true,
+  short_sale   : true,
+}
+
+const cool = new House(objek)
 
 console.log(cool.to_s())
+console.log(objek.hasOwnProperty('has_tenants'))
